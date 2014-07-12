f294b40f7dd1e4562d52ee3e9937ad889b87b6f3
Rob Johansen <robert_johans22@hotmail.com>
2014-07-12 15:53:59 -0600

    Commit some excellent progress on the structure and style.

--- themes/wordyintrovert/index.php
+++ themes/wordyintrovert/index.php
@@ -20,13 +20,65 @@ get_header(); ?>
         </div>
     </div>
 
+    <div id="post-content">
+        <div id="post-list">
+
 <?php
-    while (have_posts()) {
-        the_post();
-        echo '<h2>'.the_title().'</h2>';
-        the_content();
+    $post_count = 0;
+    while (have_posts() && $post_count < 10) {
+        the_post(); // Get the next post
+
+        $post_thumbnail = get_the_post_thumbnail();
+        $post_permalink = get_permalink();
+
+        if (strlen($post_thumbnail) == 0) {
+            $video_thumbnail_url = get_post_meta(get_the_ID(), 'video_thumbnail_url', true);
+            if (strlen($video_thumbnail_url) > 0) {
+                $post_thumbnail = '<iframe class="video-thumbnail" width="270" height="212" src="'.$video_thumbnail_url.'" frameborder="0"></iframe>';
+            }
+        } else {
+            $post_thumbnail = '<a href="'.$post_permalink.'" title="'.get_the_title().'">'.$post_thumbnail.'</a>';
+        }
+
+        $post_month   = get_the_date('M');
+        $post_date    = get_the_date('j');
+        $post_year    = get_the_date('Y');
+        $post_title   = get_the_title();
+        $post_content = get_the_content();
+
+echo <<<POST
+            <div class="post-snippet">
+                <div class="post-thumbnail">
+                    $post_thumbnail
+                </div>
+                <div class="post-metadata">
+                    <div class="post-date-box">
+                        <div class="post-date-box-top">
+                            $post_month $post_date
+                        </div>
+                        <div class="post-date-box-bottom">
+                            $post_year
+                        </div>
+                    </div>
+                    <div class="post-info">
+                        <div class="post-title">
+                            <a href="$post_permalink">$post_title</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+POST;
+
+        $post_count++;
     }
 ?>
+
+        </div> <!-- #post-list -->
+        <div id="post-sidebar">
+
+        </div>
+    </div> <!-- #post-content -->
+
 
 <?php
 get_footer();
